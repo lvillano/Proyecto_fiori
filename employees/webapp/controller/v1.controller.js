5ed76e69acb7ea01ae93f7b61d4555e225cd55eb
lvillano <villano.lm@gmail.com>
2021-06-12 22:44:34 +0000

    commit 01

--- employees/webapp/controller/v1.controller.js
+++ employees/webapp/controller/v1.controller.js
@@ -1,15 +1,38 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("sapui5.employees.controller.v1", {
-			onInit: function () {
+     
 
-			}
-		});
-	});
+       var main =  Controller.extend("sapui5.employees.controller.v1", {});
+         
+        var main;//
+            main.prototype.onChange = function() {
+                       var input1 = this.getView().byId("input1");
+            var input1value = input1.getValue();
+
+            if (input1value.length === 6) {
+                // input1.setDesc"ription("Ok");
+                this.getView().byId("label2").setVisible(true);
+                this.getView().byId("select2").setVisible(true);
+            } else {
+                //input1.setDescription(" not Ok");
+                this.getView().byId("label2").setVisible(false);
+                this.getView().byId("select2").setVisible(false);
+            };
+
+           }
+
+            return main;
+
+
+
+
+
+       
+    });
